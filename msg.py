64aeaa26124a55acb85aaa99863618b9ea185043
thraddash <theartist@gmail.com>
2021-02-11 18:28:06 -0800

    list channel, list members

--- msg.py
+++ msg.py
@@ -3,6 +3,7 @@
 
 import slack
 import os
+import json
 from slack.errors import SlackApiError
 from pathlib import Path
 from dotenv import load_dotenv
@@ -34,6 +35,28 @@ def emphemeral_chat():
 	except SlackApiError as e:
 		assert e.response["error"]
 
+# list channels
+def getChannels():
+	try:
+		response = client.conversations_list()
+		channels = response["channels"]
+		list_channels = list(map(lambda x: x["name"], channels))
+		print(list_channels)
+	except SlackApiError as e:
+		assert e.response["error"]
+
+# get members
+def getMembers():
+	try:
+		response = client.users_list()
+		users = response["members"]
+		user_ids = list(map(lambda x: x["id"], users))
+		print(user_ids)
+	except SlackApiError as e:
+		assert e.response["error"]
+
 if __name__ == "__main__":
 	#bot_chat()
-    emphemeral_chat()
+    #emphemeral_chat()
+    #getChannels()
+	getMembers()
